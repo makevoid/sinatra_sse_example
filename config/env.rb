f603de3f8e8d6c5da2bb6d29c2e6f11c7b575fea
Francesco 'makevoid' Canessa <makevoid@gmail.com>
2016-06-15 12:40:03 +0100

    Update env.rb

--- config/env.rb
+++ config/env.rb
@@ -1,4 +1,4 @@
 require 'bundler'
 Bundler.require :default
 
-CONNS = [] # SSE connection pool
+CONNS = [] # SSE connection pool at its minimum - for a more advanced connection pool check: https://gist.github.com/TiagoCardoso1983/591d48cfde04219f801fa7fd7966571c
